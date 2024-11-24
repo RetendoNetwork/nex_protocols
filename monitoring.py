c8356128dc75d2628369bdf8ee34cf1b73b879f5
00cedke <mathiskera2509@gmail.com>
2024-11-24 18:23:59 +0100

    Added Protocol on Monitoring.

---
+++ monitoring.py
@@ -0,0 +1,69 @@
+from enum import IntEnum
+
+from nex.endpoint_interface import EndpointInterface
+from nex.service_protocol import ServiceProtocol
+from nex.packet_interface import PacketInterface
+from nex.error import Error
+from nex_protocols.globals import Globals
+
+
+class MonitoringProtocol(IntEnum):
+    PROTOCOL_ID = 0x13
+    METHOD_PING_DAEMON = 0x1
+    METHOD_GET_CLUSTER_MEMBERS = 0x2
+
+    def __init__(self):
+        self.endpoint = EndpointInterface()
+        self.PingDaemon = None
+        self.GetClusterMembers = None
+        self.Patches = ServiceProtocol()
+        self.PatchedMethods = []
+
+    def endpoint(self) -> EndpointInterface:
+        return self.endpoint
+
+    def set_endpoint(self, endpoint: EndpointInterface):
+        self.endpoint = endpoint
+
+    def set_handler_ping_daemon(self, handler):
+        self.PingDaemon = handler
+
+    def set_handler_get_cluster_members(self, handler):
+        self.GetClusterMembers = handler
+
+    def handle_packet(self, packet: PacketInterface):
+        globals = Globals()
+        message = packet.rmc_message()
+
+        if not message.is_request or message.protocol_id != self.PROTOCOL_ID:
+            return
+
+        if self.Patches and message.method_id in self.PatchedMethods:
+            self.Patches.handle_packet(packet)
+            return
+
+        if message.method_id == self.METHOD_PING_DAEMON:
+            Monitoring.ping_daemon(packet)
+        elif message.method_id == self.METHOD_GET_CLUSTER_MEMBERS:
+            Monitoring.get_cluster_members(packet)
+        else:
+            err_message = f"Unsupported Monitoring method ID: {message.method_id:#x}"
+            err = Error(1, err_message) # TODO - Replace 1 to Core Implemented Error Code
+            globals.respond_error(packet, self.PROTOCOL_ID, err)
+            globals.logger.warning(err.message)
+
+
+class Monitoring:
+    def ping_daemon(packet: PacketInterface):
+        globals = Globals()
+        request = packet.rmc_message()
+        call_id = request.call_id
+
+        globals.respond(packet)
+        
+    def get_cluster_members(packet: PacketInterface):
+        globals = Globals()
+        request = packet.rmc_message()
+        call_id = request.call_id
+
+        globals.respond(packet)
